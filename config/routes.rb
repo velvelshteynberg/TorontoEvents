cffe8750f5adc05347536ff6d2ad545f1ba0e90c
Shane0018 <shanegroup1@gmail.com>
2021-08-29 20:52:15 -0400

    resolving merge issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "about_us#home"
-  get 'TorontoEvents/about_us', to: 'about_us#about_us'
-  get 'TorontoEvents/contact', to: 'about_us#contact_us'
+  get 'toronto_events/about_us', to: 'about_us#about_us'
+  get 'toronto_events/contact', to: 'about_us#contact_us'
 
-  post 'TorontoEvents/contact_us_form', to: 'about_us#contact_us_form', as: 'TorontoEvents_contact'
+  post 'toronto_events/contact_us_form', to: 'about_us#contact_us_form'#, as: 'TorontoEvents_contact'
   resources :events, only: [:index, :show]
 
   resources :subscribers, only: [:create]
--- db/schema.rb
+++ db/schema.rb
@@ -90,14 +90,6 @@ ActiveRecord::Schema.define(version: 2021_08_23_125805) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "random_people", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "subscribers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
