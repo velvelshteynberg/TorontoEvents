7e6bceab723efdcc18c65c75e9817f91933b0503
Velvelshteynberg <Velvelshteynberg@outlook.com>
2021-09-02 22:44:07 -0400

    working on scopes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
     def index
-    @events = Event.search('bluffing')
+        @events = Event.search(name: params[:name], address: params[:address], start_date: params[:start_date], host_organization: params[:host_organization])
     end 
 
     def new
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,17 +1,50 @@
 class Event < ApplicationRecord
+    scope :names, ->(name) { where("name LIKE ?", "% #{name} %" ) }
+    scope :address, ->(address) { where("address LIKE ?", "% #{address} %" ) }
+    scope :start_date, ->(start_date) { where("name LIKE ?", "% #{start_date} %" ) }
+    scope :host_organization, ->(host_organization) { where("name LIKE ?", "% #{host_organization} %" ) } 
+    
+    scope :eventname, ->(name) { where("name LIKE ?", "%#{name}%" ) if name}
+    def self.search(conditions)
 
-    scope :events, ->(name) { where("name LIKE ?", "% #{name} %" ) }
-    scope :events, ->(address) { where("address LIKE ?", "% #{address} %" ) }
-    scope :events, ->(start_date) { where("name LIKE ?", "% #{start_date} %" ) }
-    scope :events, ->(host_organization) { where("name LIKE ?", "% #{host_organization} %" ) }
+            #loop through the params and check which ones have been submitted
+            #if they have been submitte only then summon the corresponding scope on the event method. 
+            #if none are present then events.all
+            #look into dynamic programming(the code writes itself as it executes) 
 
-    def self.search(term1)
-        
-        if term1.empty? == false
-            @events = Event.events(term1)
-        else 
+
+        if conditions[:name]
+            @one = []
+            @one.push(Event.names(term1))
+        elsif term2.length > x
+            @two = []
+            @two.push(Event.address(term2))
+        elsif term3.length > x
+            @three = []
+            @three.push(Event.start_date(term3))
+        elsif term4.length > x
+            @four = []
+            @four.push(Event.names(term4))
+        elsif term1.length == x && term2.length == x && term3.length == x && term4.length == x
             @events = Event.all
+        end 
+        puts @one
+
+        array = [@one, @two, @three, @four]
+        @events = []
+        array.each do |variable|
+            if variable.empty? == false
+                variable.each do |individual_event|
+                        unless @events.include?(individual_event)
+                        @events.push(individual_event)
+                        end 
+                end
+            end 
         end
+
+        return @events
+
+
 
     end 
 
