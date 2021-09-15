afbc4b88c5586e2f5106bf0b35150e29b28a5b18
Velvelshteynberg <Velvelshteynberg@outlook.com>
2021-09-14 23:02:41 -0400

    changes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -51,7 +51,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    root_path
+    if resource.admin
+      home_index_path
+    else
+      root_path
+    end
   end
 
   # The path used after sign up for inactive accounts.
