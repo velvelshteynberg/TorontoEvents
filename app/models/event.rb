6ec341409e3b4499df2013d30df3e7d5b5bd0487
Shoshana01 <shanabjacobs@gmail.com>
2021-09-17 18:22:39 -0400

    working on associations and join tables

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,12 @@
 class Event < ApplicationRecord
+    has_and_belongs_to_many :attending_users,
+    join_table: 'events_users',
+    class_name: 'User',
+    inverse_of: :events_users
+    has_and_belongs_to_many :bookmarked_users, 
+    join_table: 'bookmarked_events',
+    class_name: 'User',
+    inverse_of: :bookmarked_events
+    belongs_to :user
+    belongs_to :host_organization
 end
