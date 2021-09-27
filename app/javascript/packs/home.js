3acaec12ebbeb927152c57f1f6f611c30a03b29b
Shane0018 <shanegroup1@gmail.com>
2021-09-26 23:10:32 -0400

    forms completed

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,13 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(){
     console.log(location.pathname);
-    let top = document.querySelector('#top');
-    if (location.pathname === '/') {
-        // top.style.height = "20%";
-        // top.style.color = "blue";
-        console.log("hi");
+    let header = document.querySelector('.app_header');
+    let main = document.querySelector('.app_header_main');
+    let bottom = document.querySelector('.app_header_bottom');
+    if (window.location.href === '/') {
+        console.log("else");
     } else {
         // top.style.height = "120px";
-        console.log("else")
+        console.log("hi");
+        console.log(header);
+        header.style.height = "170px";
+        header.style.padding = "30px";
+        console.log(main);
+        main.style.display = "none";
+        console.log(bottom);
+        bottom.style.display = "none";
     }        
 
 });
